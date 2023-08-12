2494cfb84bf220843ba4e32d07d5d29e0040cd17
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-12 16:05:39 +0000

    SQL View that retrieves all the students that have  a score under 80 and do not have no last meeting  or last meeting is scheduled until 30 days

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@ CREATE VIEW need_meeting AS
     SELECT name FROM students
     WHERE score < 80
     AND
-    (last_meeting IS NULL OR DATEDIFF(last_meeting, INTERVAL 1 MONTH + CURDATE()) < 30);
+    (last_meeting IS NULL OR DATEDIFF(last_meeting, INTERVAL 1 MONTH + CURDATE()) <= 30);
