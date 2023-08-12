349911aef0c90142e8ca8faeb98d2cef22042e9e
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-12 12:39:50 +0000

    Create an index on the table  using the first name character & the score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 -- 9-index_name_score.sql
 -- Create an index on the table `names` using the first name character and the score
-CREATE INDEX idx_name_first ON names (name(1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
