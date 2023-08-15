a3d357026ae945793518f4f3f504de791456be3a
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-15 10:40:37 +0000

    python function that list all documents of a mongodb collection if any

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 '''8-all module'''
-
-from typing import List
-from pymongo import MongoClient
 from pymongo.collection import Collection
 from pymongo.cursor import CursorType
 
 
 def list_all(mongo_collection: Collection) -> CursorType:
+    '''
+    Lists all the documents of a MongoDB collection
+    Arg:
+        mongo_collection: a MongoDB Collection
+    Return:
+        a Python list of collection's documents if any
+    '''
     return [] if mongo_collection.count == 0\
         else [doc for doc in mongo_collection.find()]
